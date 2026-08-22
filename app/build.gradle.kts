98fa143231a261740699b9097e6fc1fc17f0f97a
chekento <152346316+chekento@users.noreply.github.com>
2026-08-22 21:19:52 +0200

    chore: version 0.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "cloud.kosch.scenicpath"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.4.2"
+        versionCode = 13
+        versionName = "0.4.3"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
 
         buildConfigField(
