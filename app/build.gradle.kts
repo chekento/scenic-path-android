3825d02e94def3fe212c9d6783591e94ec227448
chekento <152346316+chekento@users.noreply.github.com>
2026-08-23 01:22:57 +0200

    chore: bump Scenic Path to v0.4.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "cloud.kosch.scenicpath"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "0.4.7"
+        versionCode = 18
+        versionName = "0.4.8"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
 
         buildConfigField(
